6face75086e2783b3c20c9241614b1c6e4b6caef
khade <khade@khadeja_ibrahim>
2025-10-30 14:13:28 +0200

    admin things

---
+++ src/main/java/dao/AdminDAO.java
@@ -0,0 +1,52 @@
+package dao;
+
+
+import java.sql.SQLException;
+
+import modl.Admin;
+import modl.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+	public class AdminDAO {
+
+		private Connection con;
+
+		    public AdminDAO() throws SQLException {
+		        con = DbConnection.getConnection();
+		    }
+
+		    public  void addUser(Admin admin)  {
+
+		        String sql =  "INSERT INTO public.admins (int admin_id , String username , password ,email ) VALUES (?, ?, ?, ?)";
+		        
+		        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+		            pstmt.setInt(1, admin.getAdmin_id());
+		            pstmt.setString(2, admin.getUsername());
+		            pstmt.setString(3, admin.getPassword());
+		            pstmt.setString(4, admin.getEmail());
+
+		            pstmt.executeUpdate();
+		        } catch (SQLException e) {
+		            e.printStackTrace();
+		        }
+		    }
+		    public void searchUserByNameandid(String name, int id)  {
+
+		        String sql = "SELECT * FROM public.users WHERE name = ? AND id = ?";
+		        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+		            pstmt.setString(1, name);
+		            pstmt.setInt(2, id);
+		            ResultSet rs = pstmt.executeQuery();
+		            while (rs.next()) {
+		                System.out.println("User Found: " + rs.getString("name") + ", Email: " + rs.getString("email"));
+		            }
+		        } catch (SQLException e) {
+		            e.printStackTrace();
+		}
+		}
+
+	}
+	
---
+++ src/main/java/modl/Admin.java
@@ -0,0 +1,53 @@
+package modl;
+
+/*
+ * @author: your name
+ * @param Admin class to represent an admin in the system
+ * @param admin_id int unique identifier for the admin
+ * @param username String username of the admin
+ * @param password String password of the admin
+ * @param email String email of the admin
+ * @methods: getters and setters for each field
+ * @methods: constructor to initialize all fields
+ */
+public class Admin {
+	
+	
+	
+	 int admin_id;
+	    String username;
+	    String password;
+	    String email;
+
+	    public Admin(int admin_id, String username, String password, String email) {
+	        this.admin_id = admin_id;
+	        this.username = username;
+	        this.password = password;
+	        this.email = email;
+	    }
+	    public  int getAdmin_id() {
+	        return admin_id;
+	    }
+	    public String getUsername() {
+	        return username;
+	    }
+	    public String getPassword() {
+	        return password;
+	    }
+	    public String getEmail() {
+	        return email;
+	    }
+	    public void setAdmin_id(int admin_id) {
+	        this.admin_id = admin_id;
+	    }
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+}
