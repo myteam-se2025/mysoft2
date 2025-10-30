a0b00398db05286c59b538de5138823a78725f0d
Masa22-QN <s12217682@stu.najah.edu>
2025-10-30 11:03:50 +0300

    faha

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -46,7 +46,21 @@ public class UserDAO {
             e.printStackTrace();
 }
 }
-    
+  
+    public void searchUserByNameandid2(String name, int id)  {
+
+        String sql = "SELECT * FROM public.users WHERE name = ? AND id = ?";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setInt(2, id);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("User Found: " + rs.getString("name") + ", Email: " + rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+}
+}
     
     
     
