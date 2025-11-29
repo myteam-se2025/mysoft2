d3c9e09ea88c51cf674ee10d3cc6079860b217f8
Masa22-QN <s12217682@stu.najah.edu>
2025-11-29 08:07:54 +0300

    mmm

---
+++ src/main/java/service/EmailService.java
@@ -0,0 +1,56 @@
+package service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Simple Gmail SMTP Email Sender
+ */
+public class EmailService {
+
+    private final String username;
+    private final String password;
+
+    public EmailService(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void sendEmail(String to, String subject, String body) {
+
+        // SMTP settings
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        // Auth
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            // Build email
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(username));
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            msg.setSubject(subject);
+            msg.setText(body);
+
+            // Send
+            Transport.send(msg);
+
+            System.out.println("Email successfully sent to " + to);
+
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            System.out.println("Email failed: " + ex.getMessage());
+        }
+    }
+}
