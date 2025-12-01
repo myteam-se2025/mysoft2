96e7fe6efa9d540418d35366f09bac65d2eb22bb
Masa22-QN <s12217682@stu.najah.edu>
2025-12-01 13:33:20 +0300

    sss

--- src/test/java/service/LibraryFineSystemTest.java
+++ src/test/java/service/LibraryFineSystemTest.java
@@ -1,5 +1,25 @@
-package service;
+/*import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
-public class LibraryFineSystemTest {
+class LibraryFineProcessorTest {
 
+    @Test
+    void testStudentFineEmailIsSent() {
+        // Arrange
+        EmailService mockEmailService = mock(EmailService.class);
+        FineCalculator calculator = new FineCalculator();
+        LibraryFineProcessor processor = new LibraryFineProcessor(mockEmailService, calculator);
+
+        FineCalculationStrategy studentStrategy = new StudentFineStrategy();
+
+        // Act
+        processor.processFine("hayasam@najah.edu", studentStrategy, 5);
+
+        // Assert
+        verify(mockEmailService, times(1))
+            .sendEmail(eq("hayasam@najah.edu"), 
+                       eq("Library Fine Notice"), 
+                       contains("Your fine is"));
+    }
 }
+*/
