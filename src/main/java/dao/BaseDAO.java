c36acf4d680d855ad8309e20b8ff2388e2794916
khade <khadejahamoodah@gmail.com>
2025-12-02 04:37:10 +0200

    fix

--- src/main/java/dao/BaseDAO.java
+++ src/main/java/dao/BaseDAO.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import dao.*;
+
 
 /**
  * Abstract base class for all DAO classes.
